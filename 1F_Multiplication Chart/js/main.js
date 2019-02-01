04d6a4eedb6707082bd303a01878195e19d34a57
Luke Cheng <t0921988961@yahoo.com.tw>
2019-02-01 17:17:32 +0800

    compelet

--- 1F_Multiplication Chart/js/main.js
+++ 1F_Multiplication Chart/js/main.js
@@ -1,1 +1,22 @@
-(function() {})();
+// console.log('hello hi');
+// (function() {
+//   let html = '';
+
+//   for (let i = 2; i <= 9; i++) {
+//     html += `<div class="white_block block color_green">
+//       <div class="white_block_title">
+//           ${i}
+//       `;
+//     for (let j = 1; j <= 9; j++) {
+//       console.log(`${i} X ${j} = ${i * j}`);
+//       html += ` <div class="white_block_font color_green">
+//          ${i} ×  ${j} ＝ ${i * j}
+//         </div>
+//       </div>`;
+//     }
+//     html += `<div>`;
+//   }
+
+//   const list = document.querySelector('.wrap');
+//   list.innerHTML += html;
+// })();
